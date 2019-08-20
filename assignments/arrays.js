52870c07f830717e2586c2aa25e6e24598427afe
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-08-20 11:46:42 -0600

    updated with all arrays problems answered

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -75,7 +75,7 @@ let inventory = [
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a +  ${inventory[32].car_year} + ${inventory[32].car_make} + ${inventory[32].car_model}`)
+console.log(`Car 33 is a  ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`)
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
@@ -98,11 +98,11 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 
-let oldCars = oldCars = inventory.fliter(obj =>obj.car_year <2000);
+let oldCars = oldCars = inventory.filter(obj =>obj.car_year <2000);
 console.log(oldCars.length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = (obj=>obj.car_make === "BMW" || obj.car_make === "Audi");
-let jjson = JSON.stringify(BMWAndAudi);
+let BMWAndAudi = (obj => obj.car_make === "BMW" || obj.car_make === "Audi");
+let json = JSON.stringify(BMWAndAudi);
 console.log(json);
